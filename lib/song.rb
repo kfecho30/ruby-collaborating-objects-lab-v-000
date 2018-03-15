c6a7c45301ae263c44c0de26b79b9f9a06abbe2d
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 00:12:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,6 @@ class Song
   def self.new_by_filename(file)
     new_song = Song.new(file.split(" - ")[1])
     new_song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
-    new_song
+    new_song.name
   end
 end
