22ba542e5bf61e88ea23e39490126608194b8e8f
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 00:25:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
     @list = Dir.entries(@path).select{|file| file.include?('.mp3')}
   end
   def import
-    @list.each do |file|
+    @list.uniq.each do |file|
       Song.new_by_filename(file)
     end
   end
