e01a91f7e88b919b235bd38dfc09271df532fa52
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 00:18:34 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,11 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :list
 
   def initialize(path)
     @path = path
   end
   def files
-    list = Dir["/#{path}/**/*.mp3"]
+    @list = Dir["/#{path}/**/*.mp3"]
   end
   def import
   end
